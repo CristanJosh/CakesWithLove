2a855d0ab0e8f8a2250864a9d924150f0386e0f7
andreiagbisit <agbisit.andreijoaqhimali@auf.edu.ph>
2024-12-11 06:22:57 +0800

    design update

--- views/index.php
+++ views/index.php
@@ -147,7 +147,7 @@
                 <img src="../assets/image/about-us-bg.png" alt="About Cake's with Love">
             </div>
             <div class="content">
-                <h3 class="title">About Us</h3>
+                <h2 class="heading">About Us</h2>
                 <p>Cakes with Love began in Telabastagan as a creative pursuit, spreading joy through baking. Founded in 2019, we specialize in custom-made cakes, cupcakes, and brownies for any occasion. Our creations are crafted with artistry, attention to detail, and a dedication to inclusivity, offering both traditional and vegan options. We aim to make every celebration memorable with treats made from the heart, honoring your style and story.</p>
                 <div class="icons-container">
                     <div class="icons">
